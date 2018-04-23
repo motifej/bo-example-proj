3f3055d83c73860b411caed49334a132db0a9183
ymorer <ymorer@techfinancials.com>
2018-04-23 17:18:27 +0300

    webpack dev server fix

--- webpack.dev.js
+++ webpack.dev.js
@@ -5,6 +5,7 @@ const BUILD_DIR = path.join(__dirname, "public");
 const common = require("./webpack.common.js");
 
 module.exports = merge(common, {
+    mode: "development",
     devtool: "inline-source-map",
     devServer: {
         contentBase: BUILD_DIR,
