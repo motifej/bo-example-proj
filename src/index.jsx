77d7c588fabd6688cdb97b3621057214b64038e0
esraajb <jbesraa@gmail.com>
2018-05-07 15:11:48 +0300

    add development condition for rendering

--- src/index.jsx
+++ src/index.jsx
@@ -2,11 +2,8 @@ import React from "react";
 import { render } from "react-dom";
 import App from "./App.jsx";
 
-if (!process.env.NODE_ENV || process.env.NODE_ENV === "development")
-    render(<App />, document.getElementById("root"));
-
 export default {
     show: props => {
-        return ReactDOM.render(<App />, document.getElementById("root"));
+        return ReactDOM.render(<App />, document.getElementById("app"));
     }
 };
