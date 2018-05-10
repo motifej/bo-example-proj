c3458464913a7d584b9a16221b3046e183d38f7e
esraajb <jbesraa@gmail.com>
2018-05-10 17:02:22 +0300

    submodule test

--- src/index.jsx
+++ src/index.jsx
@@ -1,6 +1,62 @@
 import React from "react";
 import { render } from "react-dom";
-import App from "./App.jsx";
+
+import {
+    RaisedPrimaryBtn,
+    RaisedDefaultBtn,
+    RaisedSecondaryBtn,
+    RaisedDsiabledBtn,
+    FlatDefaultBtn,
+    FlatPrimaryBtn,
+    FlatSecondaryBtn,
+    FlatDsiabledBtn
+} from "./bo-shared-components/src/ui/Button/index.js";
+
+import {
+    DisabledInput,
+    DefaultInput,
+    TextField
+} from "./bo-shared-components/src/ui/Input";
+
+import { BuildDefaultTable } from "./bo-shared-components/src/ui/Table";
+
+const TableConfig = {
+    domain: "http://almond:8081",
+    endPoint: "/accounts",
+    header: [
+        {
+            value: "id",
+            name: "Id"
+        },
+        {
+            value: "first_name",
+            name: "First Name"
+        }
+    ]
+};
+
+const UsersTable = BuildDefaultTable(TableConfig);
+
+const App = () => {
+    return (
+        <div>
+            <RaisedPrimaryBtn />
+            <RaisedDefaultBtn />
+            <RaisedSecondaryBtn />
+            <RaisedDsiabledBtn />
+            <FlatDefaultBtn />
+            <FlatPrimaryBtn />
+            <FlatSecondaryBtn />
+            <FlatDsiabledBtn />
+            <DisabledInput />
+            <DefaultInput />
+            <TextField />
+            <UsersTable />
+        </div>
+    );
+};
+if (!process.env.NODE_ENV || process.env.NODE_ENV === "development")
+    render(<App />, document.getElementById("app"));
 
 export default {
     show: props => {
